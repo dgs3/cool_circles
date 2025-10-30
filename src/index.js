c637b3bf96ca29a3928b3464609e84c35d6cbe17
Dave Sayles <sayles.dave@gmail.com>
2025-10-29 22:51:16 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import p5 from 'p5';
+import WanderingEllipse from './wandering_ellipse.js';
 
 const sketch = (p) => {
-  let fixedSize = 1000;
+  let fixedSize = 100;
   // Scaling factor for the entire sketch.
   let scaleUnit;
   // Graphics object used to draw everything.
   let pg;
-  let el_width = 100;
-  let el_height = 100;
+  let ellipses = [];
 
   p.setup = () => {
     p.randomSeed($fx.rand() * 1000000);
@@ -25,18 +25,37 @@ const sketch = (p) => {
     // Allows for crisper images on retina displays.
     pg.pixelDensity(2);
     console.log(p.random(0, 100));
+    // Initialize a collection of wandering ellipses
+    const count = 10;
+    const neonPalette = [
+      { r: 57, g: 255, b: 20 },   // neon green
+      { r: 0, g: 255, b: 255 },   // cyan
+      { r: 255, g: 20, b: 147 },  // deep pink
+      { r: 255, g: 0, b: 255 },   // magenta
+      { r: 255, g: 255, b: 0 },   // yellow
+      { r: 80, g: 200, b: 255 }   // electric blue
+    ];
+    for (let i = 0; i < count; i++) {
+      const size = 100 - (10 * i);
+      const color = neonPalette[i % neonPalette.length];
+      ellipses.push(new WanderingEllipse(size, size, 0, 0, color));
+    }
   };
 
   p.draw = () => {
-    el_width += p.random(-2, 2);
-    el_height += p.random(-2, 2);
+    // Update ellipse sizes over time
+    for (let i = 0; i < ellipses.length; i++) {
+      ellipses[i].iterate(p);
+    }
     pg.push();
     pg.clear();
     // Start drawing at the center of the screen.
     pg.translate(pg.width / 2, pg.height / 2);
     // Scale the sketch up/down to allow for dimensionless.
     pg.scale(scaleUnit);
-    pg.ellipse(el_width, el_height, p.width/2, p.height/2);
+    for (let i = 0; i < ellipses.length; i++) {
+      ellipses[i].draw(pg);
+    }
     pg.pop();
     // Draw the graphics buffer to screen.
     p.image(pg, -p.width / 2, -p.height / 2);
